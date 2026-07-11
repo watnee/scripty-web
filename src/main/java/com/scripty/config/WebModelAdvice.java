838fd2065a1417d372aea5203df204493c013537
watnee <clintonwatnee@gmail.com>
2026-07-11 09:59:01 -0400

    Bump default asset version to 193 to invalidate browser cache

--- src/main/java/com/scripty/config/WebModelAdvice.java
+++ src/main/java/com/scripty/config/WebModelAdvice.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class WebModelAdvice {
 
-    @Value("${app.asset-version:192}")
+    @Value("${app.asset-version:193}")
     private String assetVersion;
 
     @Value("${app.service-worker-enabled:false}")

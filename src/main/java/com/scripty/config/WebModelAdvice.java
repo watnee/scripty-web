d169b56ce6bfebe0c3278bff23932389b88f703b
watnee <clintonwatnee@gmail.com>
2026-07-12 01:02:13 -0400

    Style view button blue on project page and bump asset version

--- src/main/java/com/scripty/config/WebModelAdvice.java
+++ src/main/java/com/scripty/config/WebModelAdvice.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class WebModelAdvice {
 
-    @Value("${app.asset-version:203}")
+    @Value("${app.asset-version:204}")
     private String assetVersion;
 
     @Value("${app.service-worker-enabled:false}")
